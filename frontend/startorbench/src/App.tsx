81e42a78dbfaf47ea030ac75b9bd41c7571aeda2
samimu7 <samikhishali@gmail.com>
2023-11-20 03:28:08 +0100

    added fixture component

--- frontend/startorbench/src/App.tsx
+++ frontend/startorbench/src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Fixture, Lineup, Player, getFixtures, getLineup } from "./api";
+import FixtureList from "./FixtureList";
+import LineupComponent from "./LineupComponent";
 
 function App() {
   const [fixtures, setFixtures] = useState<Fixture[]>([]);
@@ -8,13 +10,31 @@ function App() {
   const [lineup, setLineup] = useState<Lineup | null>(null);
 
   useEffect(() => {
-    getFixtures().then((data) => setFixtures(data));
+    async function fetchData() {
+      try {
+        const fixturesData = await getFixtures();
+        setFixtures(fixturesData);
+      } catch (error) {
+        console.error("Error fetching fixtures:", error);
+      }
+    }
+
+    fetchData();
   }, []);
 
   useEffect(() => {
-    if (selectedFixture) {
-      getLineup(selectedFixture.fixtureId).then((data) => setLineup(data));
+    async function fetchLineup() {
+      if (selectedFixture) {
+        try {
+          const lineupData = await getLineup(selectedFixture.fixtureId);
+          setLineup(lineupData);
+        } catch (error) {
+          console.error("Error fetching lineup:", error);
+        }
+      }
     }
+
+    fetchLineup();
   }, [selectedFixture]);
 
   const handleFixtureClick = (fixture: Fixture) => {
@@ -26,12 +46,13 @@ function App() {
     player.votesToBench = (player.votesToBench || 0) + 1;
     setLineup((prevLineup) => {
       if (!prevLineup) return null;
-      return {
+      const updatedLineup = {
         ...prevLineup,
         [selectedFixture?.teamOne.includes(player) ? "teamOne" : "teamTwo"]: prevLineup[selectedFixture?.teamOne.includes(player) ? "teamOne" : "teamTwo"].map((p) =>
           p.playerId === player.playerId ? player : p
         ),
       };
+      return updatedLineup;
     });
   };
 
@@ -40,51 +61,12 @@ function App() {
       <header className="App-header">
         <h1>ShouldBeBenched</h1>
       </header>
-      <div className="fixture-list">
-        <h2>Fixtures</h2>
-        <ul>
-          {fixtures.map((fixture) => (
-            <li key={fixture.id} onClick={() => handleFixtureClick(fixture)} className="fixture-item">
-              {fixture.teamOne} vs {fixture.teamTwo}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="lineup">
-        {selectedFixture && (
-          <>
-            <h2>Lineup</h2>
-            <div className="team">
-              <h3>{selectedFixture.teamOne}</h3>
-              <ul>
-                {lineup?.teamOne.map((player) => (
-                  <li key={player.playerId} className="player-item">
-                    <span className="player-name">{player.name}</span>
-                    <button className="vote-button" onClick={() => handlePlayerVote(player)}>
-                      Should be benched
-                    </button>
-                    <span className="votes">{player.votesToBench || 0} votes</span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="team">
-              <h3>{selectedFixture.teamTwo}</h3>
-              <ul>
-                {lineup?.teamTwo.map((player) => (
-                  <li key={player.playerId} className="player-item">
-                    <span className="player-name">{player.name}</span>
-                    <button className="vote-button" onClick={() => handlePlayerVote(player)}>
-                      Should be benched
-                    </button>
-                    <span className="votes">{player.votesToBench || 0} votes</span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </>
-        )}
-      </div>
+      <FixtureList fixtures={fixtures} onFixtureClick={handleFixtureClick} />
+      <LineupComponent
+        selectedFixture={selectedFixture}
+        lineup={lineup}
+        onPlayerVote={handlePlayerVote}
+      />
     </div>
   );
 }
